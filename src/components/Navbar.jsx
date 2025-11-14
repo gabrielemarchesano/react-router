85bade7220519e015477e4f016537f15c5abe6cf
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-11-14 14:22:35 +0100

    create navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+export default function Navbar() {
+
+  const linkItems = [
+    {
+      id: 1,
+      path: "/",
+      body: "Home"
+    },
+    {
+      id: 2,
+      path: "/about",
+      body: "About Us"
+    },
+    {
+      id: 3,
+      path: "/products",
+      body: "Products"
+    }
+  ]
+
+  return (
+    <>
+      <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/"><i className="bi bi-bullseye fs-1"></i></a>
+          <div class="items">
+            <ul class="navbar-nav flex-row gap-4">
+              {
+                linkItems.map((item) => (
+                  <li class="nav-item" key={item.id}>
+                    <a class="nav-link" aria-current="page" href={item.path}>{item.body}</a>
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  )
+}
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,9 @@
+import Navbar from "../components/Navbar";
+
 export default function Homepage(){
   return(
     <>
+      <Navbar />
       <h1>Benvenuto nel Boolstore!</h1>
     </>
   )
