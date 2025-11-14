29c20bfc823a4bf28384bed9a06a0d6526d26251
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-11-14 13:50:26 +0100

    style products list

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -19,16 +19,30 @@ export default function ProductsPage(){
   return(
     <>
       <ul>
-        {
-          products.map((product) => (
-            <li>
-              <img width={100} src={product.image} alt="" />
-              <h5>{product.title}</h5>
-              <small>Price: {product.price}$</small>
-              <p>{product.description}</p>
-            </li>
-          ))
-        }
+        <div className="row row-cols-1 row-cols-md-3">
+          {
+            products.map((product) => (
+              <li className="list-unstyled" key={product.id}>
+          
+                <div className="card rounded-0">
+                  <div className="border-bottom border-1">
+                    <img src={product.image} alt=""className="card-img-top product-image"/>
+                  </div>
+                  <div className="card-body">
+                    <div className="card-title">
+                      <h5>{product.title}</h5>
+                      <small>Price: {product.price}$</small>
+                    </div>
+                    <p>{product.description}</p>
+                    <p>{product.category}</p>
+                    <p>{product.rating.rate}</p>
+                    <p>{product.rating.count}</p>
+                  </div>
+                </div>
+              </li>
+            ))
+          }
+        </div>
       </ul>
     </>
   )
