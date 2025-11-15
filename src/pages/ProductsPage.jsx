48c22dc831ee162f0bf1975d552236e23f276357
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-11-15 17:05:36 +0100

    products list style

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-export default function ProductsPage(){
-  
+export default function ProductsPage() {
+
   const productsEndpoint = "https://fakestoreapi.com/products";
 
   const [products, setProducts] = useState([]);
@@ -16,27 +16,30 @@ export default function ProductsPage(){
       })
   }, [])
 
-  return(
+  return (
     <>
       <ul>
-        <div className="row row-cols-1 row-cols-md-3">
+        <div className="row row-cols-1 row-cols-md-3 py-4 row-gap-4">
           {
             products.map((product) => (
               <li className="list-unstyled" key={product.id}>
-          
-                <div className="card rounded-0">
-                  <div className="border-bottom border-1">
-                    <img src={product.image} alt=""className="card-img-top product-image"/>
-                  </div>
-                  <div className="card-body">
-                    <div className="card-title">
+
+                <div className="card">
+                  <div className="card-header d-flex p-0 align-items-center">
+                    <div className="px-3 py-4">
+                      <img src={product.image} alt="" className="card-img-top product-image" />
+                    </div>
+                    <div className="card-title p-3">
                       <h5>{product.title}</h5>
                       <small>Price: {product.price}$</small>
                     </div>
+                  </div>
+                  <div className="card-body">
+                    <p>Category: {product.category}</p>
+                    <label htmlFor="">Description</label>
                     <p>{product.description}</p>
-                    <p>{product.category}</p>
-                    <p>{product.rating.rate}</p>
-                    <p>{product.rating.count}</p>
+                    <p>Rates: {product.rating.rate}</p>
+                    <p>Remaining: {product.rating.count} piecies</p>
                   </div>
                 </div>
               </li>
